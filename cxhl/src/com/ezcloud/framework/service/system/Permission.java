5dba9be4200ceeac62fc1434bc21bcbc87f29986
javakaka <510836102@qq.com>
2015-06-30 23:05:51 +0800

    2015-06-30
    2015-06-30

--- cxhl/src/com/ezcloud/framework/service/system/Permission.java
+++ cxhl/src/com/ezcloud/framework/service/system/Permission.java
@@ -33,7 +33,7 @@ public class Permission extends Service {
 		//直接根据人员权限获取菜单，并集
 		sql = "select DISTINCT c.FUN_ID,d.FUN_NAME,d.FUN_DESC,d.WIN_ID,d.WIN_MODE,d.ICO_NAME,d.LEVEL_INDEX,d.UP_FUN_ID,e.WIN_TARGET,e.WIN_DESC "
 				+ "from sm_staff a,sm_staff_role b ,sm_role_fun c,sm_fun d LEFT JOIN sm_window e on d.WIN_ID=e.WIN_ID " + "where a.STAFF_NO=b.STAFF_NO " + "and a.STAFF_NO='" + staff_no + "' "
-				+ "and b.ROLE_ID=c.ROLE_ID " + "and c.FUN_ID=d.FUN_ID and (b.use_state=1 or b.assign_state=1 )";
+				+ "and b.ROLE_ID=c.ROLE_ID " + "and c.FUN_ID=d.FUN_ID and (b.use_state=1 or b.assign_state=1 ) order by d.level_index ";
 		dataSet = queryDataSet(sql);
 		//根据人员所属岗位获取岗位的菜单，并集
 		if(dataSet.size() == 0)
@@ -46,7 +46,7 @@ public class Permission extends Service {
 		+" where  srf.ROLE_ID in ( "
 		+" select DISTINCT a.ROLE_ID from sm_position_role a,sm_staff_position b "  
 		+" where a.POSI_NO=b.POSI_NO and b.STAFF_NO='"+staff_no+"' and (a.use_state=1 or a.assign_state=1)"
-		+" )";
+		+" ) order by sf.level_index ";
 			System.out.println("---------------------->>"+sql);
 			dataSet = queryDataSet(sql);
 			
